b05c7ea438458218c9836a9cf0812b9e60b503c9
Taukir Lalwala <lalwalataukir@gmail.com>
2021-07-17 11:58:38 +0530

    modify code

--- Bit manipulation/Brian-kerningam's-algorithm.cpp
+++ Bit manipulation/Brian-kerningam's-algorithm.cpp
@@ -1,4 +1,4 @@
-// program to calculate number of set bits in binary representation of a number.
+// program to calculate number of set bits in binary representation of a an unsigned int.
 
 // time compexity : O(no. of set bits in binary representation of a number)
 // auxilliary space : O(1)
@@ -6,7 +6,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int countBits(int n)
+int countBits(unsigned int n)
 {
     int res = 0;
     while(n>0)
@@ -19,7 +19,7 @@ int countBits(int n)
 
 int main()
 {
-    int n;
+    unsigned int n;
     cin >> n;
     cout << countBits(n);
     return 0;
