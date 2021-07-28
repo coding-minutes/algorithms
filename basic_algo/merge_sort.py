fced65a487a90396d8f155095b6094e1369e45ee
ppulkit05 <pulkitagarwal0508@gmail.com>
2021-07-28 14:43:18 +0530

    add basic sorting algo(mergesort)

--- basic_algo/merge_sort.py
+++ basic_algo/merge_sort.py
@@ -2,41 +2,43 @@
 value = [81, 24 , 4783, 38, -37, 10]
 n=len(value)
 
-def merge(arr, s, m, e):
-    n1 = m - s + 1
-    n2 = e- m
+
+def merge(arr, s, mid, e):
+    n1 = mid - s + 1
+    n2 = e- mid
   
-    L = [0] * (n1)
-    R = [0] * (n2)
+    left = [0] * (n1)    
+    right = [0] * (n2)
   
     for i in range(0 , n1):
-        L[i] = arr[s + i]
+        left[i] = arr[s + i]
   
     for j in range(0 , n2):
-        R[j] = arr[m + 1 + j]
+        right[j] = arr[mid + 1 + j]
   
     i = 0     
     j = 0     
     k = s     
   
     while i < n1 and j < n2 :
-        if L[i] <= R[j]:
-            arr[k] = L[i]
+        if left[i] <= right[j]:
+            arr[k] = left[i]
             i += 1
         else:
-            arr[k] = R[j]
+            arr[k] = right[j]
             j += 1
         k += 1
 
     while i < n1:
-        arr[k] = L[i]
+        arr[k] = left[i]
         i += 1
         k += 1
   
     while j < n2:
-        arr[k] = R[j]
+        arr[k] = right[j]
         j += 1
         k += 1
+
 
 def mergeSort(arr,start,end):
 
