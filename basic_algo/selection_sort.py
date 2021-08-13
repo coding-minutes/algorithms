577251fa03cf5c0586a0b85a9b17e554c4b7094d
Naman Jain <namanb139j@gmail.com>
2021-08-14 00:48:29 +0530

    Added Merge sort python

--- basic_algo/selection_sort.py
+++ basic_algo/selection_sort.py
@@ -1,19 +1,19 @@
-
-
-value = [81, 24 , 4783, 38, -37, 10]
+value = [81, 24, 4783, 38, -37, 10]
 size = len(value)
 
+
 def selectionSort(array, size):
-   
+
     for i in range(size):
         min = i
 
         for j in range(i + 1, size):
-         
+
             if array[j] < array[min]:
                 min = j
-         
+
         (array[i], array[min]) = (array[min], array[i])
+
 
 selectionSort(value, size)
 print(value)
