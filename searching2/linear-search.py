a3588d0c21488263a54a6589b86e4cb6abe03961
Gordienko Nataliya <lovetoseehappiness@gmail.com>
2023-11-09 13:55:00 -0800

    add linear search

---
+++ searching2/linear-search.py
@@ -0,0 +1,15 @@
+my_list = [4,8,3,7,1,0,9]
+
+ele = 7
+
+flag = False
+
+for i in my_list:
+    if(i == ele):
+        flag = True
+        break
+
+        if(flag==True):
+            print("Element found")
+        else:
+            print("Element not found")    
