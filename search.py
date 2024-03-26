d5a5ef1276ae298dd3c2db1016930b2e3a204097
Joshua Ordiales <ordialesjd@gmail.com>
2024-03-26 11:15:54 +0800

    add linear-search2.py

---
+++ search.py
@@ -0,0 +1,15 @@
+
+my_list = [4,8,3,7,1,8,9]
+
+ele = 7
+
+flag = false
+for i in my_list:
+	of(i == ele):
+		flag = True
+		break
+
+if(flag==True):
+	print("Element Found")
+else:
+	print("Element not found")
